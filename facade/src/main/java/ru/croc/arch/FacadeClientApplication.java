7841b21ea43de9c0fb32035b70d3e2a6d3ea09c6
salavat <salavatm@gmail.com>
2017-04-14 21:18:04 +0300

    port war

--- facade/src/main/java/ru/croc/arch/FacadeClientApplication.java
+++ facade/src/main/java/ru/croc/arch/FacadeClientApplication.java
@@ -2,6 +2,8 @@ package ru.croc.arch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -9,10 +11,14 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
-public class FacadeClientApplication {
+public class FacadeClientApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(FacadeClientApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(FacadeClientApplication.class);
+    }
 }
--- registry/src/main/java/ru/croc/arch/EurekaRegistryApplication.java
+++ registry/src/main/java/ru/croc/arch/EurekaRegistryApplication.java
@@ -2,6 +2,8 @@ package ru.croc.arch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
@@ -9,10 +11,14 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaRegistryApplication {
+public class EurekaRegistryApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaRegistryApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(EurekaRegistryApplication.class);
+    }
 }
